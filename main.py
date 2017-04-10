32883edd89e89660489ecb660122b9958d036071
Isaac Samuel <getintouchwithisaac@gmail.com>
2017-04-09 20:25:05 -0400

    functional image puller/saver functions

--- main.py
+++ main.py
@@ -7,84 +7,94 @@ import re
 
 
 def save_img(url):
-	soup = BeautifulSoup(url)
+	#Once we have decided that the URL is an image, write to file
+	def write_to_img(r):
+		if r.status_code == 200:
+			with open('./pics/' + url[-8:], 'wb') as file:
+				r.raw.decode_content = True
+				shutil.copyfileobj(r.raw, file)
+			return True
+		else:
+			return False
+
+	r = requests.get(url, stream=True)
 	if re.search('(http://i.imgur.com/(?!a))(?!.*)', url):
 		#Links to an imgur page containing a link to the image
+		soup = BeautifulSoup(r.content, 'html.parser')
+
 		image_src = soup.find(itemprop="contentURL").get('src')
-		request = "http://i.imgur.com/" + image_src
+		request = "(http://i.imgur.com/" + image_src
+		r = requests.get(request, stream=True)
+		return write_to_img(r)
 
-	else if re.findall('http://i.imgur.com/(.*))(\?.*)?', url):
+	elif re.findall('(http://i.imgur.com/(.*))(\?.*)?', url):
 		#Links to an imgur page that is the image source
-		request = re.findall('http://i.imgur.com/(.*))(\?.*)?', url)
+		return write_to_img(r)
 
-	else if re.search('http://i.redd.tr/(.*))(\?.*)?', url):
+	elif re.search('(https://i.redd.tr/(.*))(\?.*)?', url):
 		#Links to Reddit image host
-		request = re.findall('http://i.redd.tr/(.*))(\?.*)?', url)
-
-	if request:
-		r = requests.get(request)
-		#Write image to a file
-		if r.status_code == 200:
-			with open('./pics/' + str(count), 'wb') as file:
-				r.raw.decode_content = True
-				shutil.copyfileobj(r.raw, f)
-			return True
+		request = re.findall('(https://i.redd.tr/(.*))(\?.*)?', url)
+		return write_to_img(r)
 
 	return False
 
-def get_pics(subreddit)
+def get_pics(subreddit):
 	i = 1
 	for post in subreddit.top(limit = 10):
 		if i > 3:
 			break
-		if save_image(post.url):
+		if save_img(post.url):
 			i += 1
-			count += 1
-
 
 
 def find_top_three_pics():
-	count = 0
 	#Removes current photos in pics subdirectory
 	for file in os.listdir('./pics'):
-		os.unlink('./pics' + file)
+		os.unlink('./pics/' + file)
 
+	#Creates subreddit instances with PRAW
 	spaceporn = reddit.subreddit('SpacePorn')
-	earthporn = reddit.subreddit('SpacePorn')
-	pic = earthporn = reddit.subreddit('SpacePorn')
+	earthporn = reddit.subreddit('EarthPorn')
+	pic = reddit.subreddit('Pic')
 
-	if args[1] == 'SPE':
-		#Get pics from /r/SpacePorn, /r/Pic, /r/EarthPorn
-		get_pics(spaceporn)
-		get_pics(earthporn)
-		get_pics(pic)
 
-	else if args[1] == 'SP':
-		#Get pics from /r/SpacePorn, /r/Pic
-		get_pics(spaceporn)
-		get_pics(pic)
+	#Stupid lack of case in Python!
+	try:
+		if sys.argv[1] == 'SPE':
+			#Get pics from /r/SpacePorn, /r/Pic, /r/EarthPorn
+			get_pics(spaceporn)
+			get_pics(earthporn)
+			get_pics(pic)
 
-	else if args[1] == 'SE':
-		#Get pics from /r/SpacePorn, /r/EarthPorn
-		get_pics(spaceporn)
-		get_pics(earthporn)
-	else if args[1] == 'EP':
-		#Get pics from /r/EarthPorn, /r/Pic
-		get_pics(earthporn)
-		get_pics(pic)
+		elif sys.argv[1] == 'SP':
+			#Get pics from /r/SpacePorn, /r/Pic
+			get_pics(spaceporn)
+			get_pics(pic)
 
-	else if args[1] == 'P':
-		#Get pics from /r/Pic
-		get_pics(pic)
-	else if args[1] == 'E':
-		#Get pics from /r/EarthPorn
-		get_pics(earthporn)
-	else if args[1] == 'S':
-		#Get pics from /r/SpacePorn
-		get_pics(pic)
-	else:
-		Print("Some problem with the keywords")
-		System.exit(1)
+		elif sys.argv[1] == 'SE':
+			#Get pics from /r/SpacePorn, /r/EarthPorn
+			get_pics(spaceporn)
+			get_pics(earthporn)
+		elif sys.argv[1] == 'EP':
+			#Get pics from /r/EarthPorn, /r/Pic
+			get_pics(earthporn)
+			get_pics(pic)
+
+		elif sys.argv[1] == 'P':
+			#Get pics from /r/Pic
+			get_pics(pic)
+		elif sys.argv[1] == 'E':
+			#Get pics from /r/EarthPorn
+			get_pics(earthporn)
+		elif sys.argv[1] == 'S':
+			#Get pics from /r/SpacePorn
+			get_pics(pic)
+		else:
+			print("Argument not recognized.")
+			sys.exit(1)
+	except IndexError:
+		print("Argument not found.")
+		sys.exit(1)
 
 
 
